407c77ca8cfd2ab10925bddd9e8cd078c023102a
Mangesh Zoke <mangesh.zoke@gmail.com>
2026-07-28 07:13:20 +0530

    first day

---
+++ Day01/first.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+
+Jupyter Notebook - 
+
+PIP : Installer for Python packages ( https://pypi.org/project/pip/)
+   57
+
