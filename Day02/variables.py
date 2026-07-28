38478ad4c376eedacd6cf31b427d706ddb48ed70
Mangesh Zoke <mangesh.zoke@gmail.com>
2026-07-28 08:58:23 +0530

    day02 code

---
+++ Day02/variables.py
@@ -0,0 +1,9 @@
+no1 = int(input("Enter first number"))
+print("First number entered is : ",no1)
+
+no2=int(input("Enter second number"))
+print("Second number entered is : ",no2)
+
+sum=no1+no2
+
+print("sum of two number : ",sum)
